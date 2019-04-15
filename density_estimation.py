5ef4f39bbca040284b08b6f8681822f9d6a48cd6
Genevieve Chafouleas <genevieve.chafouleas@gmail.com>
2019-04-15 14:22:46 -0400

    added WD and question 1.4

--- density_estimation.py
+++ density_estimation.py
@@ -14,6 +14,16 @@ import matplotlib.pyplot as plt
 import samplers as samplers
 from discriminator import Discriminator
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description='PyTorch model')
+parser.add_argument('--loss_type', type=str, default='JSD',
+                    help='loss to use; JSD WD')
+parser.add_argument('--question', type=int, default='3',
+                    help='loss to use; 3 kor 4')
+
+
+args = parser.parse_args()
 
 # plot p0 and p1
 plt.figure()
@@ -38,36 +48,77 @@ plt.clf()
 
 #######--- INSERT YOUR CODE BELOW ---#######
 directory = "model/"
-
-phi = np.linspace(-1,1, 21)
-
 num_epochs = 1000 
-x = samplers.distribution1(0)
-values = []
-for i in phi : 
-    y = samplers.distribution1(i)
-    model = Discriminator(2, 50, 512, 0) 
+
+if args.question ==3:
+    print("question 3")
+    phi = np.linspace(-1,1, 21)
+    x = samplers.distribution1(0)
+    values = []
+    for i in phi : 
+        y = samplers.distribution1(i)
+        model = Discriminator(2, 50, 512, 0) 
+        for epoch in range(num_epochs):
+            x_batch = torch.from_numpy(next(x))
+            y_batch = torch.from_numpy(next(y))
+            model.train(x_batch.type(torch.FloatTensor),y_batch.type(torch.FloatTensor), args.loss_type)
+        #torch.save(model.state_dict(), os.path.join(directory, 'best_params_'+str(i)+'.pt'))
+        x_dist = samplers.distribution1(0,10000)
+        y_dist = samplers.distribution1(i,10000)
+        x_dist_batch = torch.from_numpy(next(x_dist))
+        y_dist_batch = torch.from_numpy(next(y_dist)) 
+        x_value = x_dist_batch.type(torch.FloatTensor)
+        y_value = y_dist_batch.type(torch.FloatTensor)
+        if args.loss_type == "JSD":
+            print("JSD")
+            jsd = model.loss_JSD(model.forward(x_dist_batch.type(torch.FloatTensor)), model.forward(y_dist_batch.type(torch.FloatTensor)))
+            values.append(-jsd)
+        elif args.loss_type == "WD":
+            wd = torch.mean(model.forward(x_value) - model.forward(y_value))
+            values.append(wd)
+
+    plt.plot(phi,values, 'o-')
+    if args.loss_type == "JSD":
+        plt.ylabel("JSD")
+        plt.xlabel("phi")
+        plt.title("JSD vs phi")
+        plt.savefig(directory + '_JSD_phi.png', bbox_inches='tight')
+    elif args.loss_type == "WD":
+        plt.ylabel("Wasserstein Distance")
+        plt.xlabel("Phi")
+        plt.title("WD vs. Phi")
+        plt.savefig(directory + '_WD_phi.png', bbox_inches='tight')
+    plt.clf()    
+
+if args.question == 4:
+    f1_dist = samplers.distribution3(512)
+    f0_dist = samplers.distribution4(512)
+    model = Discriminator(1, 50, 512, 0)
     for epoch in range(num_epochs):
-        x_batch = torch.from_numpy(next(x))
-        y_batch = torch.from_numpy(next(y))
-        model.train(x_batch.type(torch.FloatTensor),y_batch.type(torch.FloatTensor), "WD")
-    #torch.save(model.state_dict(), os.path.join(directory, 'best_params_'+str(i)+'.pt'))
-    x_dist = samplers.distribution1(0,10000)
-    y_dist = samplers.distribution1(i,10000)
-    x_dist_batch = torch.from_numpy(next(x_dist))
-    y_dist_batch = torch.from_numpy(next(y_dist)) 
-    x_value = x_dist_batch.type(torch.FloatTensor)
-    y_value = y_dist_batch.type(torch.FloatTensor)
-    wd = torch.mean(model.forward(x_value) - model.forward(y_value))
-    values.append(wd)
+        f1_batch = torch.from_numpy(next(f1_dist))
+        f0_batch = torch.from_numpy(next(f0_dist))
+        model.train(f1_batch.type(torch.FloatTensor),f0_batch.type(torch.FloatTensor))
+    f1_value = []
+    discriminator_outputs = []
+    for batch in xx: 
+        f0 = samplers.distribution3(np.abs(int(batch)))
+        x = next(f0)
+        f0_batch = torch.from_numpy(next(f0))
+        x = f0_batch.type(torch.FloatTensor)
+        output = model(x)
+        f1 = (x * output)/(1 - output)
+        f1_value.append(f1)
+        discriminator_outputs.append(output)
+
+    plt.plot(f1_value, 'o-')
+    plt.plot(discriminator_outputs, 'o-')
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.title("Discriminator output and estimated f1")
+    plt.legend(labels = ["F1", "D(x)"])
+    plt.savefig(directory + '_question_4.png', bbox_inches='tight')
 
 
-plt.plot(phi,values, 'o-')
-plt.ylabel("Wasserstein Distance")
-plt.xlabel("Phi")
-plt.title("WD vs. Phi")
-plt.savefig(directory + '_WD_phi.png', bbox_inches='tight')
-plt.clf()
 
 
 
--- discriminator.py
+++ discriminator.py
@@ -1,6 +1,7 @@
 import torch 
 import torch.nn as nn
 import torch.optim as optim 
+from torch.autograd import grad
 
 import numpy as np
 from torch.autograd import Variable
@@ -45,7 +46,7 @@ class Discriminator(nn.Module):
         x = self.out(x)
         return x
 
-    def train(self, x, y, type_loss = "JSD"):
+    def train(self, x, y, type_loss = "None"):
         self.optimizer.zero_grad()
         x_prediction = self.forward(x)
         y_prediction = self.forward(y)
@@ -53,23 +54,34 @@ class Discriminator(nn.Module):
         loss = 0; 
         if type_loss == "JSD":
             loss = self.loss_JSD(x_prediction, y_prediction)
-        else:         
+        elif type_loss == "WD":         
             loss = self.loss_WD(x_prediction, y_prediction, self.Get_z_value(x,y, self.batch_size))
+        else : 
+            loss = self.loss(x_prediction, y_prediction)
 
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
+    def loss(self, x_pred, y_pred):
+        return - (torch.mean(torch.log(x_pred)) + torch.mean(torch.log(y_pred)))
+
     def loss_JSD(self, x_pred, y_pred):
         return -(torch.log(torch.tensor([[2.]])) + 1/2*torch.mean(torch.log(x_pred) + torch.log((1- y_pred))))
 
     def loss_WD(self, x_pred, y_pred, norm):
-        return -torch.mean(x_pred - y_pred - (self.lamda * ((norm -1)**2)))
+        return -(torch.mean(x_pred) - torch.mean(y_pred) - (self.lamda * torch.mean(((norm -1)**2))))
 
     def Get_z_value(self, x, y, size):
         a = torch.empty(size,1).uniform_(0,1)
         z_value =  a*x+ (1-a)*y
         z_value.requires_grad = True
-        out = self.forward(z_value)
-        out.mean().backward()
-        return (z_value.grad.data.norm(2))
+        out_interp = self.forward(z_value)
+        gradients = grad(outputs=out_interp, inputs=z_value,
+                   grad_outputs=torch.ones(out_interp.size()),
+                   retain_graph=True, create_graph=True, only_inputs=True)[0]
+
+        # Mean/Expectation of gradients
+        gradients = gradients.view(gradients.size(0),  -1)
+        gradient_norm = gradients.norm(2, dim=1)
+        return gradient_norm
